f1f093abbd5298f6115b6f630f889aabb14b489e
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-08 01:11:22 +0300

    clock_if_entarch.vhd

--- Arm9v/clock_if_entarch.vhd
+++ Arm9v/clock_if_entarch.vhd
@@ -17,7 +17,7 @@
 --
 --  Description   : Models a standard interface to the clock resources
 --                  of the PEX.
---
+--  https://www.micro-semiconductor.sg/datasheet/07-PEX8311RDK.pdf
 ------------------------------------------------------------------------
 
 ------------------------------- Glossary -------------------------------
