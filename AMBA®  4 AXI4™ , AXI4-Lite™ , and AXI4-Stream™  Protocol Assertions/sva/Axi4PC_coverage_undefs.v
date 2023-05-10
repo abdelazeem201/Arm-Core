498b37d8b1adaf0862f94bd906654191648314b4
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-05-10 13:32:51 +0300

    Add files via upload

---
+++ AMBA®  4 AXI4™ , AXI4-Lite™ , and AXI4-Stream™  Protocol Assertions/sva/Axi4PC_coverage_undefs.v
@@ -0,0 +1,73 @@
+//============================================================================
+//  The confidential and proprietary information contained in this file may
+//  only be used by a person authorised under and to the extent permitted
+//  by a subsisting licensing agreement from ARM Limited.
+//  
+//                 (C) COPYRIGHT 2010 ARM Limited.
+//                        ALL RIGHTS RESERVED
+// 
+//  This entire notice must be reproduced on all copies of this file
+//  and copies of this file may only be made by a person if such person is
+//  permitted to do so under the terms of a subsisting license agreement
+//  from ARM Limited.
+//
+//----------------------------------------------------------------------------
+//  Version and Release Control Information:
+//
+//  File Revision       : 91333
+//
+//  Date                :  2010-06-14 10:40:40 +0100 (Mon, 14 Jun 2010)
+//
+//  Release Information : BP063-VL-70004-r0p0-00rel0
+//
+//----------------------------------------------------------------------------
+//  Purpose             :  Undef macros for AXI4PC coverage
+//                          
+//============================================================================
+`ifdef VPE_ACE4_LITE_COVERAGE_MACROS_SV
+`undef VPE_ACE4_LITE_COVERAGE_MACROS_SV
+`undef AXI4PC_COVERAGE_DECLARATION_MACRO 
+
+`undef AXI4PC_COVERAGE_INITIAL  
+
+`undef AXI4PC_COVERAGE_CYCLE   
+
+`undef AXI4PC_COVERAGE_p_ReadCam_1 
+ 
+`undef AXI4PC_COVERAGE_p_ReadCam_reset 
+ 
+`undef AXI4PC_COVERAGE_p_ReadCam_2 
+
+`undef AXI4PC_COVERAGE_p_ReadCamPop_1 
+
+`undef AXI4PC_COVERAGE_p_ReadCamPop_2 
+
+`undef AXI4PC_COVERAGE_p_ReadCamPop_3 
+
+`undef AXI4PC_COVERAGE_p_ReadCam_3 
+
+`undef AXI4PC_COVERAGE_p_ReadCam_4 
+
+`undef AXI4PC_COVERAGE_p_ReadCam_5 
+
+`undef AXI4PC_COVERAGE_p_ReadCam_6 
+
+`undef AXI4PC_COVERAGE_p_WriteCam_1 
+
+`undef AXI4PC_COVERAGE_p_WriteCam_reset 
+
+`undef AXI4PC_COVERAGE_p_WriteCamMatchB 
+
+`undef AXI4PC_COVERAGE_p_WriteCamPopB_1 
+
+`undef AXI4PC_COVERAGE_p_WriteCamPopB_2 
+
+`undef AXI4PC_COVERAGE_p_WriteCamPopB_3 
+
+`undef AXI4PC_COVERAGE_p_WriteCamWValid 
+
+`undef AXI4PC_COVERAGE_p_WriteCamAWVALID 
+
+`undef AXI4PC_COVERAGE_CheckStrb 
+
+`endif
